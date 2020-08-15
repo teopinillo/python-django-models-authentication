0bd3d4f96f7a02f8458290c1fd9e60de072d02b1
Teo Pinillo <teopinillo@gmail.com>
2020-08-16 00:37:56 +0100

    fixed bug in models.py

--- mainapp/models.py
+++ mainapp/models.py
@@ -3,7 +3,7 @@ from django.conf import settings
 
 class BlogPost (models.Model):
     title = models.CharField ( max_length = 200, unique= True)
-    author = models.ForeignKey (settings.AUTH_USER_MODEL, related_name="post", on_delete=models.CASCADE)
+    author = models.ForeignKey (settings.AUTH_USER_MODEL, related_name="post", on_delete=models.CASCADE )
     body = models.TextField()
     postdate = models.DateTimeField(auto_now_add=True, blank=True)
 
