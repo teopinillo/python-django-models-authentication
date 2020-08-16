d97e6621c009969116850dba676a9453aeeb999e
Teo Pinillo <teopinillo@gmail.com>
2020-08-16 01:08:20 +0100

    fixed models bug 3

--- mainapp/models.py
+++ mainapp/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.conf import settings
+from django.urls import reverse
 
 class BlogPost (models.Model):
     title = models.CharField ( max_length = 200, unique= True)
